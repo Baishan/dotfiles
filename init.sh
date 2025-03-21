73b2d0b538d567aa489c2515d8a1e8906c9cd56d
Brian <brianshan@gmail.com>
2025-03-21 23:34:12 +0000

    Remove plugins

--- init.sh
+++ init.sh
@@ -6,7 +6,10 @@ ln -s ${SCRIPT_DIR}/.tmux.conf ~/.tmux.conf
 ln -s ${SCRIPT_DIR}/.zshrc ~/.zshrc
 ln -s ${SCRIPT_DIR}/powerlevel10k ~/.powerlevel10k
 ln -s ${SCRIPT_DIR}/.p10k.zsh ~/.p10k.zsh
-ln -s ${SCRIPT_DIR}/fzf ~/.fzf
+ln -s ${SCRIPT_DIR}/.zsh ~/.zsh
 
-~/.fzf/install
+if [ ! -d "~/.fzf/" ]; then
+    ln -s ${SCRIPT_DIR}/fzf ~/.fzf
+    ~/.fzf/install
+fi
 
