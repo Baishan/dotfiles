f92a18f7f1af3af4715b5bf466cd8e7589cf859b
Brian <brianshan@gmail.com>
2025-03-21 23:09:09 +0000

    fzf powerlevel

--- init.sh
+++ init.sh
@@ -2,4 +2,11 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ln ${SCRIPT_DIR}/.tmux.conf ~/.tmux.conf
+ln -s ${SCRIPT_DIR}/.tmux.conf ~/.tmux.conf
+ln -s ${SCRIPT_DIR}/.zshrc ~/.zshrc
+ln -s ${SCRIPT_DIR}/powerlevel10k ~/.powerlevel10k
+ln -s ${SCRIPT_DIR}/.p10k.zsh ~/.p10k.zsh
+ln -s ${SCRIPT_DIR}/fzf ~/.fzf
+
+~/.fzf/install
+
